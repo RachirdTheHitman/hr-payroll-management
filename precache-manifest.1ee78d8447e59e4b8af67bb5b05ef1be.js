0e4d63a3bdaabe7933f98cc7b55301317d58ed1e
Richard <hyacinthzy1@gmail.com>
2019-11-06 18:15:19 +1100

    Updates

---
+++ precache-manifest.1ee78d8447e59e4b8af67bb5b05ef1be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7964cbe8e4313ff939990debb436c0b",
+    "url": "/hr-payroll-management/index.html"
+  },
+  {
+    "revision": "60a2e40a5db8b04c3dbc",
+    "url": "/hr-payroll-management/static/css/2.34a0b89d.chunk.css"
+  },
+  {
+    "revision": "4167d13a10f15303cd8d",
+    "url": "/hr-payroll-management/static/css/main.31157b6f.chunk.css"
+  },
+  {
+    "revision": "60a2e40a5db8b04c3dbc",
+    "url": "/hr-payroll-management/static/js/2.b56e076e.chunk.js"
+  },
+  {
+    "revision": "4167d13a10f15303cd8d",
+    "url": "/hr-payroll-management/static/js/main.fb9aefab.chunk.js"
+  },
+  {
+    "revision": "0bf1e5bd2d0436dee7cd",
+    "url": "/hr-payroll-management/static/js/runtime-main.9e9efe37.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/hr-payroll-management/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hr-payroll-management/static/media/icons.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hr-payroll-management/static/media/icons.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hr-payroll-management/static/media/icons.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hr-payroll-management/static/media/icons.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hr-payroll-management/static/media/icons.fee66e71.woff"
+  }
+]);
